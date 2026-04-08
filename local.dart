1b663f6ae9a5a6ea861e47036e65e6d8456bfeac
Ivan.M <msv530ivan@gmail.com>
2026-04-08 23:02:30 +0300

    second commit

--- local.dart
+++ local.dart
@@ -1,18 +1,30 @@
 class itemName {
-  itemName(this.en, this.ru);
+  itemName(this.en, this.ru, this.es);
 
   final String en;
   final String ru;
+  final String es;
 
   static String lan = "en";
 
-  String get localization => (lan == "en") ? this.en : this.ru;
+  String get localization {
+    switch (lan) {
+      case "ru":
+        return this.ru;
+      case "es":
+        return this.es;
+      default:
+        return this.en;
+    }
+  }
 }
 
 void main() {
-  final loop = itemName("loop", "цикл");
+  final loop = itemName("loop", "цикл", "bucle");
 
   print(loop.localization);
   itemName.lan = "ru";
+  print(loop.localization);
+  itemName.lan = "es";
   print(loop.localization);
 }
