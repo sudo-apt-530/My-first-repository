5088dd8306e783a55a716b168bde54001fb0663b
Ivan.M <msv530ivan@gmail.com>
2026-04-09 19:38:48 +0300

    added George language

--- local.dart
+++ local.dart
@@ -1,9 +1,10 @@
 class itemName {
-  itemName(this.en, this.ru, this.es);
+  itemName(this.en, this.ru, this.es, this.ge);
 
   final String en;
   final String ru;
   final String es;
+  final String ge;
 
   static String lan = "en";
 
@@ -13,6 +14,8 @@ class itemName {
         return this.ru;
       case "es":
         return this.es;
+      case "ge":
+        return this.ge;
       default:
         return this.en;
     }
@@ -20,11 +23,13 @@ class itemName {
 }
 
 void main() {
-  final loop = itemName("loop", "цикл", "bucle");
+  final loop = itemName("loop", "цикл", "bucle", "ციკლი");
 
   print(loop.localization);
   itemName.lan = "ru";
   print(loop.localization);
   itemName.lan = "es";
+  print(loop.localization);
+  itemName.lan = "ge";
   print(loop.localization);
 }
